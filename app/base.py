3ad1ad6533aa8d6de1e67394e7e1ec2416d9edb6
root <root@ubuntu-1gb-sfo2-01.localdomain>
2017-04-10 16:47:52 +0000

    Added Session Persistance, CORS Support Cookies

--- app/base.py
+++ app/base.py
@@ -1,14 +1,16 @@
-from flask import Flask
-from flask import request
-from flask import session
-from flask import jsonify
+from flask import Flask, request, session, jsonify
+
+from flask_session import Session
 from flask_cors import CORS
 
 from Servicenow import Servicenow
 
 app = Flask(__name__)
 app.secret_key = 'SECRET_KEY'
-CORS(app)
+SESSION_TYPE = 'filesystem'
+app.config.from_object(__name__)
+Session(app)
+CORS(app, supports_credentials=True)
 
 def get_auth(user, passwd):
     SN = Servicenow()
@@ -25,11 +27,11 @@ def index():
            return jsonify('User not Authenticated')
        return jsonify('User Authenticated, please POST with query')
     if request.method == 'POST':
-       SN = get_auth(session['username'], session['passwd'])
+       SN = get_auth(session.get('username'), session('passwd'))
        r = SN.get('?sysparm_query=' + request.form['query'])
        return jsonify(r.json())
     if request.method == 'PUT':
-       SN = get_auth(session['username'], session['passwd'])
+       SN = get_auth(session.get('username'), session('passwd'))
        items = [item['sys_id'] for item in SN.get('?sysparm_query=' + request.json['query']).json()['result']]
        data = request.json['data']
        for _id in items:
